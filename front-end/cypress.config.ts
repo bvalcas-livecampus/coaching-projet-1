f9857c65977959f16c8f59542ff31e698f4ef425
Valcasara Bryan <31510683+ValcasaraBryan@users.noreply.github.com>
2025-02-09 13:06:16 +0100

    fix: cypress config

--- front-end/cypress.config.ts
+++ front-end/cypress.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3000",
+    specPattern: [
+      'cypress/e2e/**/*.cy.js',
+      '__tests__/e2e-tests/**/*.cy.js',
+    ],
+    excludeSpecPattern: [
+      'cypress/e2e/**/1-getting-started/**',
+      'cypress/e2e/**/2-advanced-examples/**',
+    ],
   },
 });
